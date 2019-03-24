d761c05e43b914a33f0703fec1d74869a07fe221
LNV <tshl20141@gmail.com>
2019-03-25 01:23:50 +0300

    style

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+    PopUpHide();
+});
+    //Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+}
+    //Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+}
