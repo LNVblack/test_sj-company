bae8089498c06016f7eea96d354d9ac5a36ddfcd
LNV <tshl20141@gmail.com>
2019-03-25 01:45:09 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function PopUpShow(){
 function PopUpHide(){
     $("#popup1").hide();
 }
+function CheckPerInt(){
+    var checkbox = document.getElementById("checkboxI");
+    var buttonshow = document.getElementById("applicationButton");
+    if (checkbox.checked == true){
+        buttonshow.style.display = "block";
+    } else{
+        buttonshow.style.display = "none";
+    }
+}
